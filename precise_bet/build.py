f40719b7e685858440fc822094e84230ec36058f
LTFan <xfqwdsj@qq.com>
2023-12-05 22:32:52 +0800

    build(Windows): 使用哈希作为版本

--- precise_bet/build.py
+++ precise_bet/build.py
@@ -1,4 +1,5 @@
 #  Copyright (C) 2023  LTFan (aka xfqwdsj). For full copyright notice, see `main.py`.
+import hashlib
 import os
 
 import PyInstaller.__main__
@@ -13,7 +14,9 @@ def full_build():
 
 
 def build():
-    create_versionfile(output_file='version_file.txt', version=precise_bet.__version__,
+    split_version = precise_bet.__version__.split('-')
+    version = f'{split_version[0]}.{int(hashlib.sha256(split_version[1].encode()).hexdigest(), 16) % 1000000}'
+    create_versionfile(output_file='version_file.txt', version=version,
                        company_name=precise_bet.__author__, file_description='Precise Bet', internal_name='Precise Bet',
                        legal_copyright=precise_bet.__copyright__, original_filename='precise_bet.exe',
                        product_name='Precise Bet', translations=[0x0804, 1200])

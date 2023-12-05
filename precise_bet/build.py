103e810855c0f1ad3064c972138f0d228a1a30bb
LTFan <xfqwdsj@qq.com>
2023-12-05 23:13:04 +0800

    build: 更新版本处理

--- precise_bet/build.py
+++ precise_bet/build.py
@@ -1,23 +1,23 @@
 #  Copyright (C) 2023  LTFan (aka xfqwdsj). For full copyright notice, see `main.py`.
+
 import hashlib
 import os
 
 import PyInstaller.__main__
+import semver
 from pyinstaller_versionfile import create_versionfile
 
 import precise_bet
 
 
-def full_build():
-    os.system('poetry dynamic-versioning')
-    build()
-
-
 def build():
-    split_version = precise_bet.__version__.split('-')
-    version = f'{split_version[0]}.{int(hashlib.sha256(split_version[1].encode()).hexdigest(), 16) % 1000000}'
-    create_versionfile(output_file='version_file.txt', version=version,
-                       company_name=precise_bet.__author__, file_description='Precise Bet', internal_name='Precise Bet',
+    os.system('poetry dynamic-versioning')
+    semver_version = semver.Version.parse(precise_bet.__version__)
+    version = semver_version.replace(prerelease=None, build=None)
+    version_hash = hashlib.sha256(f'{semver_version.prerelease}{semver_version.build}'.encode()).hexdigest()
+    version = f'{version}.{int(version_hash, 16) % 1000000}'
+    create_versionfile(output_file='version_file.txt', version=version, company_name=precise_bet.__author__,
+                       file_description=f'PreciseBet {precise_bet.__version__}', internal_name='PreciseBet',
                        legal_copyright=precise_bet.__copyright__, original_filename='precise_bet.exe',
-                       product_name='Precise Bet', translations=[0x0804, 1200])
+                       product_name='PreciseBet', translations=[0x0804, 1200])
     PyInstaller.__main__.run(['main.spec', '--noconfirm'])

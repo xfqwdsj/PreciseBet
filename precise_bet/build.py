3327fb598f282468a4a7c544f470f1a02dec26d9
LTFan <xfqwdsj@qq.com>
2023-12-05 23:51:03 +0800

    build: 更新版本处理

--- precise_bet/build.py
+++ precise_bet/build.py
@@ -2,6 +2,7 @@
 
 import hashlib
 import os
+import sys
 
 import PyInstaller.__main__
 import semver
@@ -12,10 +13,16 @@ import precise_bet
 
 def build():
     os.system('poetry dynamic-versioning')
+    # 使用命令方式调用，使动态版本在代码中更新的值生效
+    os.system(f'{sys.executable} -c "from precise_bet import build; build.start_building()"')
+
+
+def start_building():
+    # 要使 `__version__` 为最新值，需使用命令方式重新调用本方法
     semver_version = semver.Version.parse(precise_bet.__version__)
     version = semver_version.replace(prerelease=None, build=None)
-    version_hash = hashlib.sha256(f'{semver_version.prerelease}{semver_version.build}'.encode()).hexdigest()
-    version = f'{version}.{int(version_hash, 16) % 1000000}'
+    version_hash = int(hashlib.sha256(f'{semver_version.prerelease}{semver_version.build}'.encode()).hexdigest(), 16)
+    version = f'{version}.{version_hash}'
     create_versionfile(output_file='version_file.txt', version=version, company_name=precise_bet.__author__,
                        file_description=f'PreciseBet {precise_bet.__version__}', internal_name='PreciseBet',
                        legal_copyright=precise_bet.__copyright__, original_filename='precise_bet.exe',

65ce8d8614ab07d836aa898e0f3f6cf7f99b7f78
LTFan <xfqwdsj@qq.com>
2023-12-24 16:36:43 +0800

    build: 可生成单文件

--- precise_bet/build.py
+++ precise_bet/build.py
@@ -6,27 +6,41 @@ from pathlib import Path
 
 import PyInstaller.__main__
 
+working_dir = Path(__file__).parent
+
 
 def build():
-    working_dir = Path(__file__).parent
+    start_building(False)
 
+
+def build_one_file():
+    start_building(True)
+
+
+def start_building(one_file: bool):
     os.system('poetry dynamic-versioning')
+
     # 使用命令方式调用，使动态版本在代码中更新的值生效
-    os.system(f'{sys.executable} -c "from precise_bet import build; build.start_building(r\'{working_dir}\')"')
+    os.system(f'{sys.executable} -c "from precise_bet import build; build._build(r\'{working_dir}\', {one_file})"')
 
 
-def start_building(working_dir: str):
+def _build(working_dir_str: str, one_file: bool):
     """要使 `__version__` 为最新值，需使用命令方式重新调用本方法"""
 
-    working_dir = Path(working_dir)
+    working_dir_str = Path(working_dir_str)
 
-    py_file = working_dir / 'buildfile.py'
-    spec_file = working_dir / 'buildfile.spec'
+    py_file = working_dir_str / 'buildfile.py'
+    spec_file = working_dir_str / 'buildfile.spec'
     if spec_file.exists():
         spec_file.unlink()
     if py_file.exists():
-        py_file.rename(working_dir / 'buildfile.spec')
+        py_file.rename(working_dir_str / 'buildfile.spec')
     if not spec_file.exists():
         raise FileNotFoundError('未找到 buildfile.spec 文件')
-    PyInstaller.__main__.run([str(working_dir / 'buildfile.spec'), '--noconfirm'])
-    spec_file.rename(working_dir / 'buildfile.py')
+
+    cmd = [str(working_dir_str / 'buildfile.spec'), '--noconfirm']
+    if one_file:
+        cmd.append('-F')
+
+    PyInstaller.__main__.run(cmd)
+    spec_file.rename(working_dir_str / 'buildfile.py')

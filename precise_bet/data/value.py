d13f79ab3854a9fdfeaf188b0df9593b2fdd70dd
LTFan <xfqwdsj@qq.com>
2024-06-15 20:16:57 +0800

    feat: 更改球队价值匹配规则

--- precise_bet/data/value.py
+++ precise_bet/data/value.py
@@ -1,29 +1,36 @@
-#  Copyright (C) 2023  LTFan (aka xfqwdsj). For full copyright notice, see `main.py`.
+#  Copyright (C) 2024  LTFan (aka xfqwdsj). For full copyright notice, see `main.py`.
 
-import regex
+import re
 
 from precise_bet import rprint
 from precise_bet.util import request_content
 
 
 def get_team_value(team_id: int, ua: str) -> int:
-    url = 'https://liansai.500.com/team/' + str(team_id)
+    url = "https://liansai.500.com/team/" + str(team_id)
 
-    rprint(f'正在获取代号为 [bold]{team_id}[/bold] 的球队价值信息...')
+    rprint(f"正在获取代号为 [bold]{team_id}[/bold] 的球队价值信息...")
 
     text = request_content(url, ua)
 
-    name = regex.search(r'<h2 class="lsnav_qdnav_name">(.+)</h2>', text)
+    name = re.search(r'<h2 class="lsnav_qdnav_name">(.+)</h2>', text)
     if name:
-        rprint(f'球队名称为 [bold blue]{name.group(1)}[/bold blue]')
+        rprint(f"球队名称为 [bold blue]{name.group(1)}[/bold blue]")
     else:
-        rprint('[bold yellow]未找到球队名称')
+        rprint("[bold yellow]未找到球队名称")
 
-    rprint('正在匹配...')
+    rprint("正在匹配...")
 
-    value = regex.search(r'球队身价：&euro; (\d+)万', text)
-    if value is None:
-        rprint('[bold yellow]匹配失败。将该球队价值设为 0')
+    match = re.search(r"球队身价：(\d+)([万|亿])欧", text)
+    if match is None:
+        rprint("[bold yellow]匹配失败。将该球队价值设为 0")
         return 0
 
-    return int(value.group(1))
+    rprint(f"匹配到{match.group(0)}")
+
+    value = int(match.group(1))
+
+    if match.group(2) == "亿":
+        value *= 10000
+
+    return value

e4cfcb39b85b9950aeb689f194e8c0c6ef23d548
LTFan <xfqwdsj@qq.com>
2024-07-24 22:31:05 +0800

    feat: Okooo 样式

--- precise_bet/cli/okooo.py
+++ precise_bet/cli/okooo.py
@@ -16,8 +16,11 @@ from precise_bet.cli.export import (
     calibri,
     center,
     middle,
+    nine_point,
     odd_background_color,
+    result_color,
     ten_point,
+    ya_hei,
 )
 from precise_bet.type import Column, DataTable, MatchTable
 from precise_bet.util import mkdir, request_content
@@ -96,6 +99,11 @@ def okooo(
         style.apply(lambda _: sp_win_style, subset=[OkoooDataTable.sp_win])
         style.apply(lambda _: sp_draw_style, subset=[OkoooDataTable.sp_draw])
         style.apply(lambda _: sp_lose_style, subset=[OkoooDataTable.sp_lose])
+        style.apply(
+            lambda _: [f"{ya_hei}{result_color}{nine_point}{center}{middle}"]
+            * len(data),
+            subset=[OkoooDataTable.result],
+        )
 
         writer = pd.ExcelWriter(save_path)
         style.to_excel(writer)
@@ -157,8 +165,9 @@ def parse(project_path: Path, html: str) -> DataTable:
 
         score = tds[5].text.strip().replace("\n", " ")
 
-        result_text = tds[10].text.strip()
+        sp_tags: List[Tag] = tds[9].find_all("span")
 
+        result_text = tds[10].text.strip()
         if result_text == "0":
             result = "负"
         elif result_text == "1":
@@ -168,8 +177,6 @@ def parse(project_path: Path, html: str) -> DataTable:
         else:
             result = ""
 
-        sp_tags: List[Tag] = tds[9].find_all("span")
-
         data.loc[match_id] = OkoooDataTable.generate_row(
             volume_number=volume_number,
             match_number=match_number,
@@ -178,10 +185,10 @@ def parse(project_path: Path, html: str) -> DataTable:
             host_name=host,
             score=score,
             guest_name=guest,
-            result=result,
             sp_win=float(sp_tags[0].text),
             sp_draw=float(sp_tags[1].text),
             sp_lose=float(sp_tags[2].text),
+            result=result,
         )
 
     return data
@@ -197,7 +204,7 @@ class OkoooDataTable(MatchTable):
     host_name = Column("主队名称", str)
     score = Column("比分", str)
     guest_name = Column("客队名称", str)
-    result = Column("结果", str)
     sp_win = Column("胜", float)
     sp_draw = Column("平", float)
     sp_lose = Column("负", float)
+    result = Column("结果", str)
